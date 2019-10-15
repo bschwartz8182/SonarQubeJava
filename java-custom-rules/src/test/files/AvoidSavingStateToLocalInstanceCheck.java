3e0ae02f03d197c674ed49027cf630b237fa2869
bschwartz8182 <billschwartz@maximus.com>
2019-10-15 12:48:34 -0400

    test

--- java-custom-rules/src/test/files/AvoidSavingStateToLocalInstanceCheck.java
+++ java-custom-rules/src/test/files/AvoidSavingStateToLocalInstanceCheck.java
@@ -1,28 +1,8 @@
-
-/**
- * Example of creating a file locally
- */
-
 public class WriteStateLocally {
     
-	public void setResults() {
+	public void setResults() {  
 
-		// Do some processing here...
-		// Saving state locally
-		File file = new File("/home/myAppFolder/firstresults.txt"); // Noncompliant
-		
-		FileWriter fileWriter = null;
-		fileWriter = new FileWriter("/home/myAppFolder/secondresults.txt"); //Noncompliant
-		fileWriter.write(65);
-		
-		fop = new FileOutputStream(file); //Noncompliant
+		File file = new File(); // Noncompliant
 
-		
-		if (!file.exists()) {
-			file.createNewFile();
-		}
-	
 	}
-	
-    
 }
